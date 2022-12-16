6780af41066829a1ee51e87859543e444e438eeb
CodesForPublish <120683603+CodesForPublish@users.noreply.github.com>
2022-12-17 00:12:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import heart from './8996-heart.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={heart} className="App-logo" alt="logo" />
+        <p>
+          İremsu'ya aşığım.
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
